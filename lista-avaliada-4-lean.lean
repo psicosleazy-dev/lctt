8bd09580b7f10b241e5cccadf01bba8830991971
Your Name <you@example.com>
2023-04-12 23:24:48 -0300

    lista4 em curso

--- lista-avaliada-4-lean.lean
+++ lista-avaliada-4-lean.lean
@@ -14,18 +14,51 @@ variable R : U → U → Prop
 
 
 /-
-
 QUESTÃO 1: Complete a prova das seguintes deduções.
 
+theorem q1 : ((X ∧ X) → Y) →  ¬ Y → ¬ X :=
+begin
+  assume h: ((X ∧ X) → Y),
+  assume i: X,
+  apply h (apply and.intro i i)
+
+
+end
 -/
 
-theorem q1 : ((X ∧ X) → Y) →  ¬ Y → ¬ X := sorry
 
-theorem q2 : (X ∨ (Z ∧ X)) →  X := sorry
+theorem q2 : (X ∨ (Z ∧ X)) → X :=
+begin
+  intro h,
+  apply or.elim h,
+  apply (assume j:X, j),
+  apply (assume k:Z ∧ X, and.elim_right k)
+end
 
-theorem q3 : (∀ x:U, P x → Q x → R x x) → (∀ y:U, P y ∧ Q y) → (∀ z:U, R z z) := sorry
+--variables (α:Type*)
 
-theorem q4 : (∃ x:U, P x → ¬ Q x) → (∀ y:U, P y) → (∃ z:U, ¬ Q z) := sorry
+
+theorem q3 : (∀ x:U, P x → Q x → R x x) → (∀ y:U, P y ∧ Q y) → (∀ z:U, R z z) :=
+  assume h: (∀ x:U, P x → Q x → R x x),
+  assume i: (∀ y:U, P y ∧ Q y),
+  assume z:U,
+  have hz: P z → Q z → R z z, from h z,
+  have iz: P z ∧ Q z, from i z,
+  show R z z, from (hz (and.elim_left iz)) (and.elim_right iz)
+
+
+
+
+
+theorem q4 : (∃ x:U, P x → ¬ Q x) → (∀ y:U, P y) → (∃ z:U, ¬ Q z) := 
+  assume h: (∃ x:U, P x → ¬ Q x),
+  assume i: (∀ y:U, P y),
+  exists.elim h
+  (assume a:U, 
+  assume h2: P a → ¬ Q a,
+  have i2: P a, from i a,
+  show ∃ z:U, ¬ Q z, from exists.intro a (h2 i2))
+
 
 
 
@@ -58,14 +91,57 @@ QUESTÃO 2: Complete a prova dos seguintes teoremas:
 
 -/
 
-theorem q5 : ∀a:ℕ, soma 0 a = a := sorry
+theorem q5 : ∀a:ℕ, soma 0 a = a := 
+begin
+intro a,
+induction a,
+-- caso a = 0
+unfold soma,
+-- caso a = succ x
+rename a_n x,
+rename a_ih hi,
+refl,
+end
 
-theorem q6 : ∀a:ℕ, soma a 0 = a := sorry
+theorem q6 : ∀a:ℕ, soma a 0 = a :=
+begin
+intro a,
+induction a,
+-- caso a = 0
+unfold soma,
+-- caso a = succ x
+rename a_n x,
+rename a_ih hi,
+unfold soma,
+rw hi,
+end
 
-theorem q7 : ∀n:ℕ, vezes n 1 = n := sorry
+/-
+theorem q7 : ∀n:ℕ, vezes n 1 = n :=
+begin
+intro n,
+induction n,
+-- caso n = 0
+unfold vezes,
+-- caso n = succ x
+rename n_n m,
+rename n_ih hi,
+end
+-/
 
-theorem q8 : ∀a:ℕ, ∀b:ℕ,  soma a b = soma b a := sorry
 
+/-
+theorem q8 : ∀a:ℕ, ∀b:ℕ,  soma a b = soma b a :=
+begin
+intro a,
+induction a,
+-- caso a = 0
+  intro b,
+  unfold soma,
+-- caso a = succ x
+
+end
+-/
 
 /-
 
